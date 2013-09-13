e2f607cbb673e029a898cead1e21d8c4758fae07
Samuli Seppänen <samuli@openvpn.net>
2013-09-13 19:17:27 +0000

    Added support for excluding the module when using Hiera
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,9 @@
 # Empty class to allow including this module
 #
 class localuser {
+
+# Rationale for this is explained in init.pp of the sshd module
+if hiera('manage_localuser') != 'false' {
     # This is just a placeholder
+}
 }
